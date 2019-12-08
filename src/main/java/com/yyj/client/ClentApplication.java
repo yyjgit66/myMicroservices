13750fe12d320e6ba0b78c64568de388d6a400f4
yyjgit66 <18392562566@163.com>
2019-12-08 11:48:56 +0800

    myeclipse 提交

--- src/main/java/com/yyj/client/ClentApplication.java
+++ src/main/java/com/yyj/client/ClentApplication.java
@@ -9,5 +9,6 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 public class ClentApplication {
     public static void main(String[] args) {
 		SpringApplication.run(ClentApplication.class, args);
+		
 	}
 }
