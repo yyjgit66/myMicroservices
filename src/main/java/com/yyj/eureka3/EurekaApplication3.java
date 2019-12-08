f7cd64a9d394d3178cd70209219a261d1fecab20
yyjgit66 <18392562566@163.com>
2019-12-08 15:47:57 +0800

    eureka 第二个提交

---
+++ src/main/java/com/yyj/eureka3/EurekaApplication3.java
@@ -0,0 +1,14 @@
+package com.yyj.eureka3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaApplication3 {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaApplication3.class, args);
+	}
+}
