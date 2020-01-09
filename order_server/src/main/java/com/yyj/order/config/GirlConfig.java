ec6e7d5d139ed21022bfd0b7a91c1a1f3b3f3885
yyjgit66 <18392562566@163.com>
2020-01-09 21:01:54 +0800

    订单修改提交

---
+++ order_server/src/main/java/com/yyj/order/config/GirlConfig.java
@@ -0,0 +1,28 @@
+package com.yyj.order.config;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.stereotype.Component;
+
+@ConfigurationProperties("girl")
+@RefreshScope
+@Component
+public class GirlConfig {
+
+	private String name;
+	private Integer age;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public Integer getAge() {
+		return age;
+	}
+	public void setAge(Integer age) {
+		this.age = age;
+	}
+	
+	
+}
